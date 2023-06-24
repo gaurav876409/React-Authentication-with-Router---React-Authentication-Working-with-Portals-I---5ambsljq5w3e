8e7365a8f6675d8307d5d8493c93c41f7fd9c7af
gaurav876409 <gaurav876409@gmail.com>
2023-06-24 17:13:21 +0530

    up

--- src/components/Store.js
+++ src/components/Store.js
@@ -22,7 +22,7 @@ const Store = ({ setIsLoggedIn }) => {
 
   return (
     <div className="store">
-      <h1>Welcome to the Store</h1>
+      <h2>Welcome to the Store</h2>
       <button onClick={handleLogout}>Logout</button>
     </div>
   );
